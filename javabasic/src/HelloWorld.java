b568a4de0382ac032b49f5b013065623ea080e70
abhishek <abgoswam@gmail.com>
2016-10-28 08:09:32 -0700

    scalascala mavenhelloworld javabasic

---
+++ javabasic/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by abgoswam on 10/28/16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ scalascala/src/myApp.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by abgoswam on 10/27/16.
+  */
+object myApp {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world")
+  }
+}
